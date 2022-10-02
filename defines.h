f46eb5607265adfd23a442c8da1a902b0cd11c79
Luc3k <itslucus@gmail.com>
2022-10-02 11:01:21 +0200

    first commit

---
+++ defines.h
@@ -0,0 +1,33 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define TS 32  // TILE SIZE
+
+#define MAP_HEIGHT 14
+#define MAP_WIDTH  40
+
+#define EMPTY_TILE 0
+#define METEORITE  1
+#define WICHER     2
+
+#define DIR_NONE 0
+#define RIGHT    1
+#define LEFT     2
+#define UP       3
+#define DOWN     4
+
+#define STATE_IDLE 0
+#define STATE_ANIM 1
+
+#define FACE_RIGHT 64
+#define FACE_LEFT  96
+
+#define WICHER_ANIM_SPEED 4
+
+#define TILE_EMPTY 0
+
+
+
+
+
+#endif
